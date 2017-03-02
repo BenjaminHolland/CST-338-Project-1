8a8d69a86311fb1a8e50574ac168bc77ac291155
Ben Holland <ben.holland.core@gmail.com>
2017-03-01 19:15:26 -0800

    Commented Database.java

--- src/cst338/p1/Database.java
+++ src/cst338/p1/Database.java
@@ -9,14 +9,21 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 
+/**
+ * Educational Database.
+ * @author Benjamin
+ * Represents a database that tracks the required entities
+ */
 public class Database {
   private final Map<Integer, StudentRecord> students;
   private final Map<Integer, TeacherRecord> teachers;
   private final Map<Integer, CourseRecord> courses;
   private final Map<Integer, List<Integer>> linkStudentCourse;
   private final Map<Integer, List<Integer>> linkTeacherCourse;
-
-
+  
+  /**
+   * Initializes a new database.
+   */
   public Database() {
     students = new HashMap<>();
     teachers = new HashMap<>();
@@ -26,12 +33,23 @@ public class Database {
 
   }
 
+  /**
+   * Deletes the specified Teacher Record. 
+   * @param id The id of the teacher to delete. 
+   * @throws EntityNotFoundException If there are no teachers with the specified id.
+   * @
+   */
   public void deleteTeacher(Integer id) throws EntityNotFoundException {
     TeacherRecord teacher = getTeacherById(id);
     teachers.remove(teacher.getId());
     linkTeacherCourse.remove(teacher.getId());
   }
 
+  /**
+   * Creates the specified teacher record. 
+   * @param teacher The data to store in the teacher table.
+   * @throws EntityDuplicateException if a teacher with the same id already exists. (Possibly also if the email already exists.)
+   */
   public void createTeacher(TeacherRecord teacher) throws EntityDuplicateException {
     if (teachers.containsKey(teacher.getId())) {
       throw new EntityDuplicateException();
@@ -40,6 +58,10 @@ public class Database {
     }
   }
 
+  /**
+   * Iterate over Teachers.
+   * @return A stream of teachers.
+   */
   public Stream<TeacherRecord> getTeacherStream() {
     return teachers.values().stream();
   }
@@ -48,6 +70,12 @@ public class Database {
   // index on the email field, but as this isn't the goal of the assignment I've chosen to leave
   // it out. It also assumes that email addresses are unique, and so doesn't check for additional
   // matches.
+  /**
+   * Attempts to find a teacher by their email.
+   * @param email The email to find.
+   * @return The first teacher with a matching email in the table.
+   * @throws EntityNotFoundException If there is no teacher with a matching email.
+   */
   public TeacherRecord getTeacherByEmail(final String email) throws EntityNotFoundException {
     Optional<TeacherRecord> recordOption =
         getTeacherStream().filter(record -> record.getEmail().equals(email)).findFirst();
@@ -58,6 +86,12 @@ public class Database {
     }
   }
 
+  /**
+   * Retrieves a teacher.
+   * @param id The id of the teacher to retrieve.
+   * @return The record of the teacher with the matching id. 
+   * @throws EntityNotFoundException If there are no teachers with a matching id.
+   */
   public TeacherRecord getTeacherById(Integer id) throws EntityNotFoundException {
     TeacherRecord record = teachers.get(id);
     if (record == null) {
@@ -68,6 +102,11 @@ public class Database {
 
   }
 
+  /**
+   * Creates the specified student record in the student table. 
+   * @param record The data of the student to create. 
+   * @throws EntityDuplicateException If a student with a duplicate id is already in the database.
+   */
   public void createStudent(StudentRecord record) throws EntityDuplicateException {
     if (students.containsKey(record)) {
       throw new EntityDuplicateException();
@@ -76,6 +115,11 @@ public class Database {
     }
   }
 
+  /**
+   * Delets the student with the specified id in the student table. 
+   * @param id The id of the student to delete. 
+   * @throws EntityNotFoundException If there is no student with a matching id.
+   */
   public void deleteStudent(Integer id) throws EntityNotFoundException {
     StudentRecord record = getStudentById(id);
     // Remove record;
@@ -85,6 +129,12 @@ public class Database {
     linkStudentCourse.remove((Object) record.getId());
   }
 
+  /**
+   * Retrieves the student with the specified id.
+   * @param id The id of the student to retrieve. 
+   * @return The record of the student with the specified id.
+   * @throws EntityNotFoundException If there is no student with a matching id found.
+   */
   public StudentRecord getStudentById(Integer id) throws EntityNotFoundException {
     StudentRecord record = students.get(id);
     if (record == null) {
@@ -94,10 +144,19 @@ public class Database {
     }
   }
 
+  /**
+   * Iterates over students.
+   * @return A stream of Student Records.
+   */
   public Stream<StudentRecord> getStudentStream() {
     return students.values().stream();
   }
 
+  /**
+   * Creates a course.
+   * @param course The data of the course to store in the database. 
+   * @throws EntityDuplicateException If there's already a course with a matching id.
+   */
   public void createCourse(CourseRecord course) throws EntityDuplicateException {
     if (courses.containsKey(course.getId())) {
       throw new EntityDuplicateException();
@@ -107,6 +166,11 @@ public class Database {
 
   }
 
+  /**
+   * Deletes the specified course.
+   * @param id The id of the course to delete. 
+   * @throws EntityNotFoundException if there is no course with a matching id. 
+   */
   public void deleteCourse(Integer id) throws EntityNotFoundException {
     CourseRecord record = getCourseById(id);
     courses.remove(record.getId());
@@ -118,6 +182,12 @@ public class Database {
     }
   }
 
+  /**
+   * Retrieves a specified course.
+   * @param id The id of the course record to retireve. 
+   * @return The Course Record with a matching id. 
+   * @throws EntityNotFoundException If there was no course with a matching id. 
+   */
   public CourseRecord getCourseById(Integer id) throws EntityNotFoundException {
     CourseRecord record = courses.get(id);
     if (record == null) {
@@ -127,8 +197,15 @@ public class Database {
     }
   }
 
+  /**
+   * Specify that the given teacher is assigned to a given course.
+   * @param teacher The teacher to assign to the course.
+   * @param course The course to assign the teacher to.
+   * @throws EntityDuplicateException if the teacher is already assigned to the class.
+   * @throws EntityNotFoundException If the specified Teacher or Course is not already in the database. 
+   */
   public void linkTeacherCourse(TeacherRecord teacher, CourseRecord course)
-      throws EntityDuplicateException {
+      throws EntityDuplicateException,EntityNotFoundException {
     if (!linkTeacherCourse.containsKey(teacher.getId())) {
       linkTeacherCourse.put(teacher.getId(), new ArrayList<>());
     }
@@ -139,6 +216,12 @@ public class Database {
     }
   }
 
+  /**
+   * Specify the given teacher is no longer assigned to a given course.
+   * @param teacher The teacher to remove from the course.  
+   * @param course The course to remove the teacher from.
+   * @throws EntityNotFoundException If the teacher is not assigned to the course, or the specified course or teacher is not in the database. 
+   */
   public void unlinkTeacherCourse(TeacherRecord teacher, CourseRecord course)
       throws EntityNotFoundException {
     if (!linkTeacherCourse.containsKey(teacher.getId())) {
@@ -153,10 +236,19 @@ public class Database {
   }
 
   //TODO: write link/unlink methods for Student/Course linkages.
+  /**
+   * Iterate over courses. 
+   * @return A stream of course records.
+   */
   public Stream<CourseRecord> getCourseStream() {
     return courses.values().stream();
   }
 
+  /**
+   * Import data from the specified path into the database. 
+   * @param path The path of the data file to read from.
+   * @throws IOException If there was an error reading the file.
+   */
   public void importData(String path) throws IOException {
     DataFile newData = DataFile.load(path);
 
