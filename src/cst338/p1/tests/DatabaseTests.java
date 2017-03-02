eaa2f71129916016ae95e92de1eec77e5b49d75b
Ben Holland <ben.holland.core@gmail.com>
2017-03-01 19:28:24 -0800

    Added skeleton for Database Tests

--- src/cst338/p1/tests/DatabaseTests.java
+++ src/cst338/p1/tests/DatabaseTests.java
@@ -7,8 +7,101 @@ import org.junit.Test;
 public class DatabaseTests {
 
   @Test
-  public void test() {
+  public void createStudent_nominal() {
     fail("Not yet implemented");
   }
+  @Test
+  public void createStudent_duplicate() {
+    fail("Not yet implemented");
+  }
+  @Test
+  public void createTeacher_nominal() {
+    fail("Not yet implemented");
+  }
+  @Test
+  public void createTeacher_duplicateId() {
+    fail("Not yet implemented");
+  }
+  @Test
+  public void createTeacher_duplicateEmail() {
+    fail("Not yet implemented");
+  }
+  @Test
+  public void createCourse_nominal() {
+    fail("Not yet implemented");
+  }
+  @Test
+  public void createCourse_duplicate() {
+    fail("Not yet implemented");
+  }
+  @Test
+  public void linkStudentCourse_nominal(){
+   fail("Not yet implemented."); 
+  }
+  @Test
+  public void linkStudentCourse_duplicate(){
+   fail("Not yet implemented."); 
+  }
+  @Test
+  public void linkStudentCourse_missingStudent(){
+   fail("Not yet implemented."); 
+  }
+  
+  @Test
+  public void linkStudentCourse_missingCourse(){
+   fail("Not yet implemented."); 
+  }
+  @Test
+  public void linkTeacherCourse_nominal(){
+   fail("Not yet implemented."); 
+  }
+  @Test
+  public void linkTeacherCourse_duplicate(){
+   fail("Not yet implemented."); 
+  }
+  @Test
+  public void linkTeacherCourse_missingTeacher(){
+   fail("Not yet implemented."); 
+  }
+  
+  @Test
+  public void linkTeacherCourse_missingCourse(){
+   fail("Not yet implemented."); 
+  }
 
+  
+  @Test
+  public void unlinkStudentCourse_nominal(){
+   fail("Not yet implemented."); 
+  }
+  @Test
+  public void unlinkStudentCourse_missing(){
+   fail("Not yet implemented."); 
+  }
+  @Test
+  public void unlinkStudentCourse_missingStudent(){
+   fail("Not yet implemented."); 
+  }
+  
+  @Test
+  public void unlinkStudentCourse_missingCourse(){
+   fail("Not yet implemented."); 
+  }
+  @Test
+  public void unlinkTeacherCourse_nominal(){
+   fail("Not yet implemented."); 
+  }
+  @Test
+  public void unlinkTeacherCourse_missing(){
+   fail("Not yet implemented."); 
+  }
+  @Test
+  public void unlinkTeacherCourse_missingTeacher(){
+   fail("Not yet implemented."); 
+  }
+  
+  @Test
+  public void unlinkTeacherCourse_missingCourse(){
+   fail("Not yet implemented."); 
+  }
 }
