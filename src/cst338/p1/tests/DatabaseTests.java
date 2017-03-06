22c3e4398921e2b8d6dd989919afea22daf7add9
Ben Holland <ben.holland.core@gmail.com>
2017-03-06 15:49:49 -0800

    Issue #4: Corrected Tests for Relationship Queries. All Tests Passing

--- src/cst338/p1/tests/DatabaseTests.java
+++ src/cst338/p1/tests/DatabaseTests.java
@@ -25,10 +25,12 @@ public class DatabaseTests {
   private final StudentRecord stdStudentRecord = new StudentRecord(2000, "A. Student");
   private final CourseRecord stdCourseRecord =
       new CourseRecord(3000, "CLS-3000 Introduction To Subject", 10, "ROOM 0");
-@Test
-  public void deleteStudent_nominal() throws EntityNotFoundException, EntityDuplicateException,CourseFullException {
-    Database db=new Database();
-    
+
+  @Test
+  public void deleteStudent_nominal()
+      throws EntityNotFoundException, EntityDuplicateException, CourseFullException {
+    Database db = new Database();
+
     // Add Student
     db.createStudent(stdStudentRecord);
     // Add Class
@@ -38,30 +40,32 @@ public class DatabaseTests {
     // Delete student
     db.deleteStudent(stdStudentRecord.getId());
     // Ensure student no longer exists.
-    assertEquals(0,db.getStudentStream().collect(Collectors.counting()).intValue());
+    assertEquals(0, db.getStudentStream().collect(Collectors.counting()).intValue());
     // Ensure class still exists.
     db.getCourseById(stdCourseRecord.getId());
     // Ensure student is no longer in class.
-    assertEquals(0,db.getStudentsForCourse(stdCourseRecord.getId()).count());
+    assertEquals(0, db.getStudentsForCourse(stdCourseRecord.getId()).count());
 
   }
-@Test
+
+  @Test
   public void deleteStudent_missing() throws EntityNotFoundException {
     // Expect an EntityNotFound Exception.
     thrown.expect(EntityNotFoundException.class);
     // Call Delete student.
-    Database db=new Database();
+    Database db = new Database();
     db.deleteStudent(stdStudentRecord.getId());
   }
-@Test
+
+  @Test
   public void deleteTeacher_nominal() throws EntityNotFoundException, EntityDuplicateException {
-    Database db=new Database();
+    Database db = new Database();
     // Add Teacher.
     db.createTeacher(stdTeacherRecord);
     // Add Class.
     db.createCourse(stdCourseRecord);
     // Link teacher to class.
-    db.linkTeacherCourse(stdTeacherRecord.getId(),stdCourseRecord.getId());
+    db.linkTeacherCourse(stdTeacherRecord.getId(), stdCourseRecord.getId());
     // Delete teacher.
     db.deleteTeacher(stdTeacherRecord.getId());
     // Ensure teacher no longer exists.
@@ -70,21 +74,24 @@ public class DatabaseTests {
     db.getCourseById(stdCourseRecord.getId());
     // Ensure teacher is no longer assigned to the class.
     assertEquals(0, db.getTeachersForCourse(stdCourseRecord.getId()).count());
- 
+
   }
-@Test
+
+  @Test
   public void deleteTeacher_missing() throws EntityNotFoundException {
     // Expect an EntityNotFound Exception.
     thrown.expect(EntityNotFoundException.class);
-    Database db=new Database();
+    Database db = new Database();
     // Call deleteTeacher
     db.deleteTeacher(stdTeacherRecord.getId());
-   
-  }
-@Test
-  public void deleteCourse_nominal() throws EntityNotFoundException, EntityDuplicateException,CourseFullException {
 
-    Database db=new Database();
+  }
+
+  @Test
+  public void deleteCourse_nominal()
+      throws EntityNotFoundException, EntityDuplicateException, CourseFullException {
+
+    Database db = new Database();
     // Add Teacher.
     db.createTeacher(stdTeacherRecord);
     // Add Student.
@@ -92,31 +99,32 @@ public class DatabaseTests {
     // Add Course.
     db.createCourse(stdCourseRecord);
     // Link Teacher to Course.
-    db.linkTeacherCourse(stdTeacherRecord.getId(),stdCourseRecord.getId());
+    db.linkTeacherCourse(stdTeacherRecord.getId(), stdCourseRecord.getId());
     // Link Student to Course.
-    db.linkStudentCourse(stdStudentRecord.getId(),stdCourseRecord.getId());
+    db.linkStudentCourse(stdStudentRecord.getId(), stdCourseRecord.getId());
     // Delete course.
     db.deleteCourse(stdCourseRecord.getId());
     // Ensure course no longer exists.
-    assertEquals(0,db.getCourseStream().count());
+    assertEquals(0, db.getCourseStream().count());
     // Ensure teacher still exists.
     db.getTeacherById(stdTeacherRecord.getId());
     // Ensure student still exists.
     db.getStudentById(stdStudentRecord.getId());
     // Ensure teacher is no longer assigned to course.
-    assertEquals(0,db.getCoursesForTeacher(stdTeacherRecord.getId()).count());
+    assertEquals(0, db.getCoursesForTeacher(stdTeacherRecord.getId()).count());
     // Ensure student is no longer in course.
-    assertEquals(0,db.getCoursesForStudent(stdStudentRecord.getId()).count());
+    assertEquals(0, db.getCoursesForStudent(stdStudentRecord.getId()).count());
 
   }
-@Test
+
+  @Test
   public void deleteCourse_missing() throws EntityNotFoundException {
-    Database db=new Database();
-    //Expect EntityNotFoundException
+    Database db = new Database();
+    // Expect EntityNotFoundException
     thrown.expect(EntityNotFoundException.class);
-    //Call deleteCourse
+    // Call deleteCourse
     db.deleteCourse(stdCourseRecord.getId());
-    
+
   }
 
 
@@ -184,7 +192,7 @@ public class DatabaseTests {
   }
 
   private void linkStudentCourse_nominal_core(Database db)
-      throws EntityNotFoundException, EntityDuplicateException,CourseFullException {
+      throws EntityNotFoundException, EntityDuplicateException, CourseFullException {
     db.createStudent(new StudentRecord(1000, "Yar Ogg"));
     db.createCourse(new CourseRecord(2000, "HIS-2000 Mastadon Hunting", 30, "CAVE-99"));
     db.linkStudentCourse(1000, 2000);
@@ -199,14 +207,15 @@ public class DatabaseTests {
   }
 
   @Test
-  public void linkStudentCourse_nominal() throws EntityDuplicateException, EntityNotFoundException,CourseFullException {
+  public void linkStudentCourse_nominal()
+      throws EntityDuplicateException, EntityNotFoundException, CourseFullException {
     Database db = new Database();
     linkStudentCourse_nominal_core(db);
   }
 
   @Test
   public void linkStudentCourse_duplicate()
-      throws EntityDuplicateException, EntityNotFoundException,CourseFullException {
+      throws EntityDuplicateException, EntityNotFoundException, CourseFullException {
     Database db = new Database();
     db.createStudent(new StudentRecord(1000, "Yar Ogg"));
     db.createCourse(new CourseRecord(2000, "HIS-2000 Mastadon Hunting", 30, "CAVE-99"));
@@ -217,7 +226,7 @@ public class DatabaseTests {
 
   @Test
   public void linkStudentCourse_missingStudent()
-      throws EntityDuplicateException, EntityNotFoundException,CourseFullException {
+      throws EntityDuplicateException, EntityNotFoundException, CourseFullException {
     Database db = new Database();
     db.createCourse(
         new CourseRecord(1000, "CST-1000 Introduction to 4th-Dimensional Computers", 10, "EXT-5"));
@@ -228,26 +237,29 @@ public class DatabaseTests {
 
   @Test
   public void linkStudentCourse_missingCourse()
-      throws EntityDuplicateException, EntityNotFoundException,CourseFullException {
+      throws EntityDuplicateException, EntityNotFoundException, CourseFullException {
     Database db = new Database();
     db.createStudent(new StudentRecord(1000, "Joe Jim Joseph Juperm"));
     thrown.expect(EntityNotFoundException.class);
     db.linkStudentCourse(1000, 2000);
 
   }
+
   @Test
-  public void linkStudentCourse_fullCourse() throws EntityDuplicateException,EntityNotFoundException,CourseFullException{
-     Database db=new Database();
-     db.createCourse(stdCourseRecord);
-     for(int idx=0;idx<10;idx++){
-       db.createStudent(new StudentRecord(idx,"Student "+idx));
-       db.linkStudentCourse(idx,stdCourseRecord.getId());
-     }
-     db.createStudent(new StudentRecord(11,"Student 11"));
-     thrown.expect(CourseFullException.class);
-     db.linkStudentCourse(11,stdCourseRecord.getId());
-     
+  public void linkStudentCourse_fullCourse()
+      throws EntityDuplicateException, EntityNotFoundException, CourseFullException {
+    Database db = new Database();
+    db.createCourse(stdCourseRecord);
+    for (int idx = 0; idx < 10; idx++) {
+      db.createStudent(new StudentRecord(idx, "Student " + idx));
+      db.linkStudentCourse(idx, stdCourseRecord.getId());
+    }
+    db.createStudent(new StudentRecord(11, "Student 11"));
+    thrown.expect(CourseFullException.class);
+    db.linkStudentCourse(11, stdCourseRecord.getId());
+
   }
+
   private void linkTeacherCourse_nominal_core(Database db)
       throws EntityNotFoundException, EntityDuplicateException {
     db.createTeacher(new TeacherRecord(1000, "Hort Mra", "hmra@rock.edu", "000-00-0498"));
@@ -291,8 +303,8 @@ public class DatabaseTests {
 
 
   }
-  
- 
+
+
   @Test
   public void linkTeacherCourse_missingCourse()
       throws EntityDuplicateException, EntityNotFoundException {
@@ -306,7 +318,7 @@ public class DatabaseTests {
 
   @Test
   public void unlinkStudentCourse_nominal()
-      throws EntityDuplicateException, EntityNotFoundException,CourseFullException {
+      throws EntityDuplicateException, EntityNotFoundException, CourseFullException {
     Database db = new Database();
     linkStudentCourse_nominal_core(db);
     db.unlinkStudentCourse(1000, 2000);
@@ -390,92 +402,113 @@ public class DatabaseTests {
     db.unlinkTeacherCourse(3000, 2);
     fail("Not yet implemented.");
   }
-  
+
   @Test
-  public void getTeachersByEmail_nominal() throws EntityDuplicateException, EntityNotFoundException{
-    Database db=new Database();
-    //Add teacher
+  public void getTeachersByEmail_nominal()
+      throws EntityDuplicateException, EntityNotFoundException {
+    Database db = new Database();
+    // Add teacher
     db.createTeacher(stdTeacherRecord);
-    //Add another teacher with a different email.
-    db.createTeacher(new TeacherRecord(1450,"B. Teacher","bteacher@school.edu","192-39-1000"));
-    //Get list of teachers with matching emails.
-    List<TeacherRecord> results=db.getTeachersByEmail(stdTeacherRecord.getEmail()).collect(Collectors.toList());
-    //Ensure there is only one teacher returned.
-    assertEquals(1,results.size());
-    //Ensure the one teacher returned has the correct email.
-    assertEquals(stdTeacherRecord.getEmail(),results.get(0).getEmail());
+    // Add another teacher with a different email.
+    db.createTeacher(new TeacherRecord(1450, "B. Teacher", "bteacher@school.edu", "192-39-1000"));
+    // Get list of teachers with matching emails.
+    List<TeacherRecord> results =
+        db.getTeachersByEmail(stdTeacherRecord.getEmail()).collect(Collectors.toList());
+    // Ensure there is only one teacher returned.
+    assertEquals(1, results.size());
+    // Ensure the one teacher returned has the correct email.
+    assertEquals(stdTeacherRecord.getEmail(), results.get(0).getEmail());
   }
+
   @Test
-  public void getTeachersByEmail_missing() throws EntityDuplicateException{
-    Database db=new Database();
-    //Add a teacher
+  public void getTeachersByEmail_missing() throws EntityDuplicateException {
+    Database db = new Database();
+    // Add a teacher
     db.createTeacher(stdTeacherRecord);
-    //Get a list of teachers with an email that is anything but the email of the teacher added.
-    List<TeacherRecord> results=db.getTeachersByEmail("email@not.matching").collect(Collectors.toList());
-    //Ensure there are no teachers returned.
-    assertEquals(0,results.size());
+    // Get a list of teachers with an email that is anything but the email of the teacher added.
+    List<TeacherRecord> results =
+        db.getTeachersByEmail("email@not.matching").collect(Collectors.toList());
+    // Ensure there are no teachers returned.
+    assertEquals(0, results.size());
   }
+
   @Test
-  public void getTeachersByEmail_duplicate() throws EntityDuplicateException{
-    Database db=new Database();
-    TeacherRecord otherTeacher=new TeacherRecord(1,"Alphabet Soup","asoup@school.edu","098-76-5432");
-    TeacherRecord dupTeacher=new TeacherRecord(2,"Another Teacher","ateacher@school.edu","101-01-0101");
-    //Add a teacher
+  public void getTeachersByEmail_duplicate() throws EntityDuplicateException {
+    Database db = new Database();
+    TeacherRecord otherTeacher =
+        new TeacherRecord(1, "Alphabet Soup", "asoup@school.edu", "098-76-5432");
+    TeacherRecord dupTeacher =
+        new TeacherRecord(2, "Another Teacher", "ateacher@school.edu", "101-01-0101");
+    // Add a teacher
     db.createTeacher(stdTeacherRecord);
-    //Add another teacher with a different email.
+    // Add another teacher with a different email.
     db.createTeacher(otherTeacher);
-    //Add another teacher with the same email.
+    // Add another teacher with the same email.
     db.createTeacher(dupTeacher);
-    //Get list of teachers with emails matching the first email.
-    List<TeacherRecord> results=db.getTeachersByEmail(stdTeacherRecord.getEmail()).collect(Collectors.toList());
-    //Ensure there are two teachers returned.
+    // Get list of teachers with emails matching the first email.
+    List<TeacherRecord> results =
+        db.getTeachersByEmail(stdTeacherRecord.getEmail()).collect(Collectors.toList());
+    // Ensure there are two teachers returned.
     assertEquals(2, results.size());
-    //Ensure the emails of those teacher match.
+    // Ensure the emails of those teacher match.
     assertTrue(results.get(0).getEmail().equals(results.get(1).getEmail()));
-    assertTrue(results.get(0)!=results.get(1)); //Reference Equals.
-    assertTrue(results.get(0).getEmail().equals(stdTeacherRecord.getEmail())); //Transitive Equality.
+    assertTrue(results.get(0) != results.get(1)); // Reference Equals.
+    assertTrue(results.get(0).getEmail().equals(stdTeacherRecord.getEmail())); // Transitive
+                                                                               // Equality.
   }
-  
+
   @Test
-  public void getTeachersForCourse_nominal() throws EntityDuplicateException, EntityNotFoundException{
-    Database db=new Database();
+  public void getTeachersForCourse_nominal()
+      throws EntityDuplicateException, EntityNotFoundException {
+    Database db = new Database();
     db.createTeacher(stdTeacherRecord);
     db.createCourse(stdCourseRecord);
-    db.linkTeacherCourse(stdTeacherRecord.getId(),stdCourseRecord.getId());
-    List<TeacherRecord> teachersForCourse=db.getTeachersForCourse(stdCourseRecord.getId()).collect(Collectors.toList());
-    assertEquals(1,teachersForCourse.size());
-    assertEquals(stdTeacherRecord.getId(),teachersForCourse.get(0).getId());
+    db.linkTeacherCourse(stdTeacherRecord.getId(), stdCourseRecord.getId());
+    List<TeacherRecord> teachersForCourse =
+        db.getTeachersForCourse(stdCourseRecord.getId()).collect(Collectors.toList());
+    assertEquals(1, teachersForCourse.size());
+    assertEquals(stdTeacherRecord.getId(), teachersForCourse.get(0).getId());
+
+  }
+
+  @Test
+  public void getCoursesForTeacher_nominal()
+      throws EntityDuplicateException, EntityNotFoundException {
+    Database db = new Database();
+    db.createTeacher(stdTeacherRecord);
+    db.createCourse(stdCourseRecord);
+    db.linkTeacherCourse(stdTeacherRecord.getId(), stdCourseRecord.getId());
+    List<CourseRecord> coursesForTeacher =
+        db.getCoursesForTeacher(stdTeacherRecord.getId()).collect(Collectors.toList());
+    assertEquals(1, coursesForTeacher.size());
+    assertEquals(stdCourseRecord.getId(), coursesForTeacher.get(0).getId());
+  }
+
+  @Test
+  public void getStudentsForCourse_nominal()
+      throws EntityDuplicateException, EntityNotFoundException, CourseFullException {
+    Database db = new Database();
+    db.createStudent(stdStudentRecord);
+    db.createCourse(stdCourseRecord);
+    db.linkStudentCourse(stdStudentRecord.getId(), stdCourseRecord.getId());
+    List<StudentRecord> studentsForCourse =
+        db.getStudentsForCourse(stdCourseRecord.getId()).collect(Collectors.toList());
+    assertEquals(1, studentsForCourse.size());
+    assertEquals(stdStudentRecord.getId(), studentsForCourse.get(0).getId());
+  }
+
+  @Test
+  public void getCoursesForStudent_nominal()
+      throws EntityNotFoundException, EntityDuplicateException, CourseFullException {
+    Database db = new Database();
+    db.createStudent(stdStudentRecord);
     
-  }
-  @Test
-  public void getCoursesForTeacher_nominal() throws EntityDuplicateException, EntityNotFoundException{
-    Database db=new Database();
-    db.createTeacher(stdTeacherRecord);
     db.createCourse(stdCourseRecord);
-    db.linkTeacherCourse(stdTeacherRecord.getId(),stdCourseRecord.getId());
-    List<CourseRecord> coursesForTeacher=db.getCoursesForTeacher(stdTeacherRecord.getId()).collect(Collectors.toList());
-    assertEquals(1,coursesForTeacher.size());
-    assertEquals(stdCourseRecord.getId(),coursesForTeacher.get(0).getId());
-  }
-  
-  @Test
-  public void getStudentsForCourse_nominal() throws EntityDuplicateException, EntityNotFoundException, CourseFullException{
-    Database db=new Database();
-    db.createStudent(stdStudentRecord);
-    db.createCourse(stdCourseRecord);
-    db.linkStudentCourse(stdStudentRecord.getId(),stdCourseRecord.getId());
-    List<StudentRecord> studentsForCourse=db.getStudentsForCourse(stdCourseRecord.getId()).collect(Collectors.toList());
-    assertEquals(1,studentsForCourse.size());
-    assertEquals(stdStudentRecord.getId(),studentsForCourse.get(0).getId()); 
-  }
-  @Test
-  public void getCoursesForStudent_nominal() throws EntityNotFoundException, EntityDuplicateException, CourseFullException{
-    Database db=new Database();
-    db.createStudent(stdStudentRecord);
-    db.createCourse(stdCourseRecord);
-    db.linkStudentCourse(stdStudentRecord.getId(),stdCourseRecord.getId());
-    CourseRecord[] coursesForStudent=(CourseRecord[])db.getCoursesForStudent(stdStudentRecord.getId()).toArray();
-    assertEquals(1,coursesForStudent.length);
-    assertEquals(coursesForStudent[0].getId(),stdCourseRecord.getId());
+    
+    db.linkStudentCourse(stdStudentRecord.getId(), stdCourseRecord.getId());
+    
+    List<CourseRecord> coursesForStudent =db.getCoursesForStudent(stdStudentRecord.getId()).collect(Collectors.toList());
+    assertEquals(1, coursesForStudent.size());
+    assertEquals(coursesForStudent.get(0).getId(), stdCourseRecord.getId());
   }
 }
