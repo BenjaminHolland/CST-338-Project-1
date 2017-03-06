979b0276f932cb4f85a3f7327fec0fea7bfae48b
Ben Holland <ben.holland.core@gmail.com>
2017-03-06 15:57:52 -0800

    Implemented School.readData

--- src/cst338/p1/EntryPoint.java
+++ src/cst338/p1/EntryPoint.java
@@ -1,7 +1,7 @@
 package cst338.p1;
 
 public class EntryPoint {
-
+  
   public static void main(String[] args) {
     // TODO Auto-generated method stub
 
--- src/cst338/p1/School.java
+++ src/cst338/p1/School.java
@@ -1,5 +1,23 @@
 package cst338.p1;
 
-public class School {
+import java.io.IOException;
 
+public class School {
+  private final Database database;
+  private final String name;
+  public String getName(){
+    return name;
+  }
+  public School(String name){
+    this.name=name;
+    this.database=new Database();
+  }
+  public void readData(String path){
+      try {
+        database.importData(path);
+      } catch (IOException e) {
+        System.out.println("Error importing data from \""+path+"\"");
+        e.printStackTrace();
+      }
+  }
 }
