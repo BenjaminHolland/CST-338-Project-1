e5d6f33cbd327238005a9c24755d1f0d32adadc2
Ben Holland <ben.holland.core@gmail.com>
2017-03-07 11:23:01 -0800

    Added implementation for Student wrapper.

--- src/cst338/p1/Course.java
+++ src/cst338/p1/Course.java
@@ -1,5 +1,12 @@
 package cst338.p1;
 
 public class Course {
-  
+  private Database context;
+  private Integer studentId;
+  private Integer courseId;
+  public Course(Database context,Integer studentId,Integer courseId){
+    this.context=context;
+    this.studentId=studentId;
+    this.courseId=courseId;
+  }
 }
--- src/cst338/p1/Student.java
+++ src/cst338/p1/Student.java
@@ -1,5 +1,40 @@
 package cst338.p1;
 
-public class Student {
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Student {
+  private final Database context;
+  private final Integer id;
+
+  Student(Database context, Integer id) {
+    this.context = context;
+    this.id = id;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public Boolean exists() {
+    try {
+      context.selectStudent(id);
+    } catch (StudentMissingException ex) {
+      return false;
+    }
+    return true;
+  }
+
+  public String getName() throws StudentMissingException {
+    return context.selectStudent(id).getName();
+  }
+
+  public List<Course> getCourses() throws StudentMissingException{
+    return context.selectStudentCourses(id).stream().map(entry->new Course(context,id,entry.getCourseId())).collect(Collectors.toList());
+  }
+  
+  public Course getCourse(Integer courseId) throws EnrollmentMissingException, CourseMissingException, StudentMissingException{  
+    context.selectStudentCourse(id, courseId); //Forces checks for enrollment. Not very nice, but works. 
+    return new Course(context,id,courseId);
+  }
 }
