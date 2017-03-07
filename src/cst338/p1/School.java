2a4c7c2a42170448c1119310433f0c650c02f865
Ben Holland <ben.holland.core@gmail.com>
2017-03-06 16:08:01 -0800

    Added stubs for required methods of School

--- src/cst338/p1/School.java
+++ src/cst338/p1/School.java
@@ -20,4 +20,49 @@ public class School {
         e.printStackTrace();
       }
   }
+  public void schoolInfo(){
+    
+  }
+  public void searchByEmail(){
+    
+  }
+  public void addInstructor(Integer id,String name,String email,String phone){
+    
+  }
+  public void addCourse(Integer id,String name,Integer capacity,String location){
+    
+  }
+  public void addStudent(Integer id,String name){
+    
+  }
+  public void assignInstructor(Integer classId,Integer teacherId){
+    
+  }
+  public void register(Integer classId,Integer studentId){
+    
+  }
+  public void unRegister(Integer classId,Integer studentId){
+    
+  }
+  public StudentRecord getStudent(Integer studentId){
+    return null;
+  }
+  public void graduateStudent(Integer studentId){
+    
+  }
+  public void assignScore(Integer classId,Integer studentId,Double score){
+    
+  }
+  public void courseInfo(Integer classId){
+    
+  }
+  public CourseRecord getCourse(Integer classId){
+    return null;
+  }
+  public void courseInfo(){
+    
+  }
+  public void deleteCourse(Integer classId){
+    
+  }
 }
